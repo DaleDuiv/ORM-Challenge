03cb9aa6f5d9735fcee73eee841cb53faac005f1
DaleDuiv <110407486+DaleDuiv@users.noreply.github.com>
2023-01-15 10:41:17 +1030

    added in code and fixed bugs

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag, as: 'products_with_this_tag' }],
+      include: [{ model: Product, through: ProductTag, as: 'tag_id' }],
     })
 
     res.status(200).json(tagData)
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: 'products_with_this_tag'}],
+      include: [{ model: Product, through: ProductTag, as: 'tag_id'}],
       });
   if (!tagData) {
     res.status(404).json({message: 'Tag not found'});
